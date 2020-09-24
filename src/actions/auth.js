f04d9d87a67c9707d859f61c1fa62de424035211
estherkang14 <estherkang14@gmail.com>
2020-09-24 12:59:54 -0400

    deleted commented out code in auth action file

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -108,37 +108,3 @@ export const storeWeather = weather => {
     }
 }
 
-// export const postDailyPost = post => {
-//     return {
-//         type: 'POST_DAILY_POST',
-//         post
-//     }
-// }
-
-// export const postEvent = event => {
-//     return {
-//         type: 'POST_EVENT',
-//         event
-//     }
-// }
-
-// export const postTask = task => {
-//     return {
-//         type: 'POST_TASK',
-//         task
-//     }
-// }
-
-// export const deleteTask = deletedTask => {
-//     return {
-//         type: 'DELETE_TASK',
-//         deletedTask
-//     }
-// }
-
-// export const getWeather = weather => {
-//     return {
-//         type: 'GET_WEATHER',
-//         weather
-//     }
-// }
